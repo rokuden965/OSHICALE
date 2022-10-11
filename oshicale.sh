55153adbc645ee391059a868e2dc1c3a99b11bac
rokuden965 <115361446+rokuden965@users.noreply.github.com>
2022-10-11 22:29:25 +0900

    Update oshicale.sh

--- oshicale.sh
+++ oshicale.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/usr/bin/zsh
 
 nohup /usr/bin/python 絶対パス/oshicale_schedule.py &!
+#バックグラウンドにて実行させる
+
+#OSHICALEシステムを動作させるときはこのシェルスクリプトを動作させてください。
+
+#止めるときはps -xコマンドで出てきたoshicale_schedule.pyのプロセス番号をkill [プロセス番号]してください。
