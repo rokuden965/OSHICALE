bfbd6d5079e2df07d1d2e48b1be659cdb702fcb4
rokuden965 <115361446+rokuden965@users.noreply.github.com>
2022-10-12 18:54:26 +0900

    Update oshicale_schedule.py

--- oshicale_schedule.py
+++ oshicale_schedule.py
@@ -4,11 +4,14 @@ import schedule
 from time import sleep
 import oshicale_account1
 
+#定期動作させるタスクを設定
 def tasks():
     oshicale_account1.main()
 
+#30分おきの動作を設定
 schedule.every(30).minutes.do(tasks)
 
+#繰り返し実行させる。
 while True:
     schedule.run_pending()
     sleep(1)
